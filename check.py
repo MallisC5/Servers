6635f05ae8ecbda6989211dd5dec44c761415222
MallisC5 <121091209+MallisC5@users.noreply.github.com>
2023-06-04 23:50:19 +0700

    Update check.py

--- check.py
+++ check.py
@@ -3,7 +3,7 @@ import threading
 # Usage: python3 checker.py threads
 def check_proxy(proxy):
     try:
-        response = requests.get('https://www.google.com/', proxies={'http': proxy, 'https': proxy}, timeout=12)
+        response = requests.get('https://www.google.com/', proxies={'http': proxy, 'https': proxy}, timeout=15)
         if response.status_code == 200:
             print(f"Proxy is working -> {proxy}")
             with open('proxy.txt', 'a') as f:
